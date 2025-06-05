be08b0d19b8e9f2b105acadb282b89d4b702856e
Divya Gaddam <HR419@sgpltech.com>
2025-06-05 12:15:38 +0530

    adding cluster files

---
+++ eks/eks_cluster.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = var.instance_types
+      desired_size   = var.desired_size
+      max_size       = var.max_size
+      min_size       = var.min_size
+    }
+  }
+
+  # Recommended for newer versions
+  enable_irsa = true
+}
---
+++ eks/variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "instance_types" {
+  description = "EC2 instance types for EKS worker nodes"
+  type        = list(string)
+}
